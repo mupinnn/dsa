564955bc58b51d770ab2f08cc8315c5a983b5ea1
Ahmad Muwaffaq <itsmupin@gmail.com>
2021-10-27 20:31:17 +0700

    feat(fcc): symmetric-diff done

--- fcc/symmetric-diff/main.js
+++ fcc/symmetric-diff/main.js
@@ -15,7 +15,25 @@
  * @returns {[]} The symmetric difference between those two arrays.
  */
 function symmetricDiff(arr1, arr2) {
-  return "not implemented";
+  const diff = [];
+
+  // Solution 1
+  // This is the first and the best solution so far
+  // that I can think of. Still finding another best
+  // solution that more simple and efficient.
+  for (let i = 0; i < arr1.length; i++) {
+    if (!arr2.includes(arr1[i])) {
+      diff.push(arr1[i]);
+    }
+  }
+
+  for (let j = 0; j < arr2.length; j++) {
+    if (!arr1.includes(arr2[j])) {
+      diff.push(arr2[j]);
+    }
+  }
+
+  return diff;
 }
 
 module.exports = symmetricDiff;
