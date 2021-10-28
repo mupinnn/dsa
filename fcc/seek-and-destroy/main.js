c62fa50116bc46d9a138aff447b8228f95eb00b7
Ahmad Muwaffaq <itsmupin@gmail.com>
2021-10-28 21:49:34 +0700

    feat(fcc): seek-and-destroy done

--- fcc/seek-and-destroy/main.js
+++ fcc/seek-and-destroy/main.js
@@ -4,13 +4,26 @@
  * Remove all elements from the inital array that are of
  * the same value as the rest of the arguments.
  *
- * @param {[]} arr - Initial array
+ * @param {[]} arr - Initial array.
  * @param  {...any} args - Arguments, list of element that want
  *                         to be removed from `arr`.
- * @returns
+ * @returns {[]} Array of element that not removed in `arr`.
  */
 function destroyer(arr, ...args) {
-  return "not implemented";
+  // Imperative way
+  const diff = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    // Below can be written as `!args.includes(arr[i])`.
+    if (args.indexOf(arr[i]) === -1) {
+      diff.push(arr[i]);
+    }
+  }
+
+  return diff;
+
+  // Declarative way
+  // return arr.filter((ar) => !args.includes(ar));
 }
 
 module.exports = destroyer;
