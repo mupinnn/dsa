a6a655792f6f42fb192cad74d9a8a5c7bbf1545f
Ahmad Muwaffaq <itsmupin@gmail.com>
2021-11-04 10:52:47 +0700

    feat(fcc): pig-latin done

--- fcc/pig-latin/main.js
+++ fcc/pig-latin/main.js
@@ -2,7 +2,7 @@
  * Pig Latin is a way of altering english words. The rules as follows:
  * - If a word begins with consonant, take the first consonant or consonant
  *   cluster, move it to the end of the word, and add `ay` to it.
- * - If a word begins witha vowel, just add `way` at the end.
+ * - If a word begins with a vowel, just add `way` at the end.
  *
  * Example:
  * - "calinfornia" -> "aliforniacay".
@@ -13,7 +13,24 @@
  * @returns {string} Translated `str` to Pig Latin string.
  */
 function translatePigLatin(str) {
-  return "not implemented";
+  const lowercased = str.toLowerCase();
+  let pigLatin = lowercased;
+
+  // check if a word begins with a vowel
+  if (/[aiueo]/.test(lowercased[0])) {
+    pigLatin += "way";
+  }
+
+  // check if a word begins with a consonant or consonant cluster
+  if (/^([^aiueo]+)/g.test(lowercased)) {
+    const matchConsonant = lowercased.match(/^([^aiueo]+)/g);
+    const consonant = matchConsonant[0];
+    const sliced = pigLatin.slice(consonant.length) + consonant + "ay";
+
+    pigLatin = sliced;
+  }
+
+  return pigLatin;
 }
 
 module.exports = translatePigLatin;
