000d6424643e5ae38785a0b14ca073999e43a6d0
Ahmad Muwaffaq <itsmupin@gmail.com>
2021-11-05 11:09:15 +0700

    feat(fcc): dna-pairing done

--- fcc/dna-pairing/main.js
+++ fcc/dna-pairing/main.js
@@ -17,7 +17,38 @@
  * @returns {string} 2D array of character and its pair.
  */
 function pairElement(str) {
-  return "not implemented";
+  // One
+  const pairs = {
+    A: "T",
+    T: "A",
+    C: "G",
+    G: "C",
+  };
+
+  return str.split("").map((el) => [el, pairs[el]]);
+
+  // Two
+  // const elements = str.split("");
+  // const paired = [];
+
+  // for (let i = 0; i < elements.length; i++) {
+  //   switch (elements[i]) {
+  //     case "A":
+  //       paired.push(["A", "T"]);
+  //       break;
+  //     case "T":
+  //       paired.push(["T", "A"]);
+  //       break;
+  //     case "C":
+  //       paired.push(["C", "G"]);
+  //       break;
+  //     case "G":
+  //       paired.push(["G", "C"]);
+  //       break;
+  //   }
+  // }
+
+  // return paired;
 }
 
 module.exports = pairElement;
