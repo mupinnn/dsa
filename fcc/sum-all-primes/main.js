6b1d9065638c786dc7ac936ebd27583a690a064b
Ahmad Muwaffaq <itsmupin@gmail.com>
2021-11-10 09:04:23 +0700

    test(fcc): sum-all-primes failing

---
+++ fcc/sum-all-primes/main.js
@@ -0,0 +1,17 @@
+/**
+ * Sum All Primes
+ *
+ * A prime number is a whole number greater than 1 with exactly two divisors:
+ * 1 and itself. For example, 2 is a prime number because it is only divisible
+ * by 1 and 2 (itself). In contrast, 4 is not prime since it is divisible by 1,
+ * 2 and 4.
+ *
+ * @param {number} num - Any numbers
+ * @returns {number} The sum of all prime numbers that are less than
+ *                   or equal to `num`.
+ */
+function sumPrimes(num) {
+  return "not implemented";
+}
+
+module.exports = sumPrimes;
---
+++ fcc/sum-all-primes/main.test.js
@@ -0,0 +1,11 @@
+const sumPrimes = require("./main");
+
+test.each([
+  [10, 17],
+  [977, 73156],
+])(
+  "sum all prime numbers that less than or equal to %i resulting %i",
+  (input, expected) => {
+    expect(sumPrimes(input)).toBe(expected);
+  }
+);
