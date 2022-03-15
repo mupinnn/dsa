d5f21621985d3361d5de407350edef5a0ec3d96c
Ahmad Muwaffaq <itsmupin@gmail.com>
2022-03-15 17:02:50 +0700

    test(hr/caesar-cipher): add edge-case big rotate factor

--- hackerrank/algo/caesar-cipher/main.test.js
+++ hackerrank/algo/caesar-cipher/main.test.js
@@ -1,8 +1,28 @@
 const caesarCipher = require("./main");
 
 test.each([
-  ["There's-a-starman-waiting-in-the-sky", 3, "Wkhuh'v-d-vwdupdq-zdlwlqj-lq-wkh-vnb"],
-  ["middle-Outz", 2, "okffng-Qwvb"]
-])(`Caesar Cipher for %s with %i letter(s) rotated should return %s`, (original, rotateFactor, encrypted) => {
-  expect(caesarCipher(original, rotateFactor)).toBe(encrypted);
-});
+  // Test case 1
+  [
+    "There's-a-starman-waiting-in-the-sky",
+    3,
+    "Wkhuh'v-d-vwdupdq-zdlwlqj-lq-wkh-vnb",
+  ],
+
+  // Test case 2
+  ["middle-Outz", 2, "okffng-Qwvb"],
+
+  // Test case 3
+  ["Pz-/aI/J`EvfthGH", 66, "Dn-/oW/X`SjthvUV"],
+
+  // Test case 4
+  [
+    "Always-Look-on-the-Bright-Side-of-Life",
+    5,
+    "Fqbfdx-Qttp-ts-ymj-Gwnlmy-Xnij-tk-Qnkj",
+  ],
+])(
+  `Caesar Cipher for %s with %i letter(s) rotated should return %s`,
+  (original, rotateFactor, encrypted) => {
+    expect(caesarCipher(original, rotateFactor)).toBe(encrypted);
+  }
+);
