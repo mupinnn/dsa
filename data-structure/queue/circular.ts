2889c8164eec26c10fc71b229ca0e79277fb2bdc
Ahmad Muwaffaq <itsmupin@gmail.com>
2022-03-30 17:17:22 +0700

    feat: initialize queue with size limit

--- data-structure/queue/circular.ts
+++ data-structure/queue/circular.ts
@@ -1,3 +1,11 @@
-class CircularQueue<QueueType> {}
+class CircularQueue<QueueType> {
+  readonly length: number;
+  readonly q: Array<QueueType>;
+
+  constructor(length: number) {
+    this.length = length;
+    this.q = Array(this.length).fill(null); // create n length array
+  }
+}
 
 export default CircularQueue;
