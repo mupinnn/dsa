0ede0c873dfef743f444840d0dc2fc29a2697c2a
Ahmad Muwaffaq <itsmupin@gmail.com>
2025-06-17 20:59:49 +0700

    feat(tle): level 1, module 1 bonus done + 1

--- tle-eliminators/level-1/module-1-cpp-basics/bonus/codeforces-is-it-a-cat.cpp
+++ tle-eliminators/level-1/module-1-cpp-basics/bonus/codeforces-is-it-a-cat.cpp
@@ -1,58 +1,30 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-// TODO: still error.
 // @see https://codeforces.com/problemset/problem/1800/A
 int main() {
-  int t, n;
-  string s;
-
+  int t;
   cin >> t;
 
   for (int i = 0; i < t; i++) {
+    int n;
+    string s;
+
     cin >> n >> s;
-    transform(s.begin(), s.end(), s.begin(), ::tolower);
 
-    char prev;
-    bool meow = false;
+    string res = "";
+    int current = 0;
 
-    for (int j = 0; j < n; j++) {
-      meow = true;
+    while (current < s.size()) {
+      char c = tolower(s[current]);
+      res += c;
 
-      if ((j == 0 && s[j] != 'm') || (j == n - 1 && s[j] != 'w') || n < 4 || s[j] == ' ' || (j > 0 && prev == 'w' && s[j] != 'w')) {
-        meow = false;
-        break;
+      while (current < s.size() && tolower(s[current]) == c) {
+        current++;
       }
-
-      if (j == 0) {
-        prev = s[j];
-        continue;
-      }
-
-      if (prev == 'm' && (s[j] == 'm' || s[j] == 'e')) {
-        prev = s[j];
-        continue;
-      }
-
-      if (prev == 'e' && (s[j] == 'e' || s[j] == 'o')) {
-        prev = s[j];
-        continue;
-      }
-
-      if (prev == 'o' && (s[j] == 'o' || s[j] == 'w')) {
-        prev = s[j];
-        continue;
-      }
-
-      if (prev == 'w' && s[j] == 'w') {
-        prev = s[j];
-        continue;
-      }
-
-      meow = false;
     }
 
-    if (meow) {
+    if (res.compare("meow") == 0) {
       cout << "YES";
     } else {
       cout << "NO";
